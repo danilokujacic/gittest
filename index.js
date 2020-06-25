13a9cfd8ccfd271c5ec576117485f4c1796c0bdc
Danilo <danilo.kujacic@bild-studio.net>
2020-06-25 09:37:10 +0200

    Master je dodao nesto u medjuvremenu

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 //Hejteri, ovo je treci branch :D <3 fafafaaf
 
 //Poceo sam da radim nesto prvo
+
+//Ja sam u medjuvremenu dodao nesto odje :) (master)
+
+//Ja sam neki branch koji nastavljam svoj rad
